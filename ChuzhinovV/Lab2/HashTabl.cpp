5400f515636b8a84e3920797779ff6afe976f5a7
Vitaly777887 <chuzhinov2015@gmail.com>
2017-04-09 18:00:45 +0300

    Update HashTabl.cpp
    переделал

--- ChuzhinovV/Lab2/HashTabl.cpp
+++ ChuzhinovV/Lab2/HashTabl.cpp
@@ -4,96 +4,99 @@
 #include <fstream>
 using namespace std;
 #define Hash_size 52
-
 struct Node
- {
+{
 	int count;
 	string value;
 	Node *next;
-	}**hashtable;
-
-unsigned int Index(string value)
- {
-	int sum = 0;
-	for (unsigned int i = 0; i < value.length(); i++)
-		sum += value[i] ;
-	return (sum % Hash_size);
-	}
-Node *Search(string value)
- {
-	Node *p= hashtable[Index(value)];
-	while (p && p->value != value)
-		 p = p->next;
-	return p;
-	}
-void Insert(string value)
- {
-	if (value != "\0")
+};
+class List
+{
+	Node **head;
+public:
+	unsigned int Index(string value)
 	{
-		Node *search = Search(value);
-		int index = Index(value);
-		if (!search)
+		int sum = 0;
+		for (unsigned int i = 0; i < value.length(); i++)
+			sum += value[i];
+		return (sum % Hash_size);
+	}
+	Node *Search(string value)
+	{
+		Node *p = head[Index(value)];
+		while (p && p->value != value)
+			p = p->next;
+		return p;
+	}
+	void Insert(string value)
+	{
+		if (value != "\0")
 		{
-			if (Node *current = new Node)
+			Node *search = Search(value);
+			int index = Index(value);
+			if (!search)
 			{
-				current->value = value;
-				current->count = 1;
-				current->next = hashtable[index];
-				hashtable[index] = current;
+				if (Node *current = new Node)
+				{
+					current->value = value;
+					current->count = 1;
+					current->next = head[index];
+					head[index] = current;
+				}
 			}
+			else
+				search->count++;
 		}
-		else
-			search->count++;
 	}
-	}
-void Print()
- {
-	cout << "The resulting hash function:" << endl;
-	int sum = 0;
-	for (int i = 0; i < Hash_size; i++)
-		 if (hashtable[i])
-		 {
-		Node *q;
-		
-			while (hashtable[i])
-			 {
-			q = hashtable[i];
-			sum += q->count;
-			cout << q->value << " = " << q->count << endl;
-			hashtable[i] = hashtable[i]->next;
-			}
-		}
-	cout << "The total amount: " << sum << endl;
-	}
-void Create()
-{
-	hashtable = new Node *[Hash_size];
-	for (int i = 0; i < Hash_size; i++)
-		hashtable[i] = NULL;
-}
-void Remove()
-{
-	for (int i = 0; i < Hash_size; i++)
-		if (hashtable[i])
-		{
-			Node *q;
-			while (hashtable[i])
+	void Print()
+	{
+		cout << "The resulting hash function:" << endl;
+		int sum = 0;
+		for (int i = 0; i < Hash_size; i++)
+			if (head[i])
 			{
-				q = hashtable[i];
-				hashtable[i] = hashtable[i]->next;
-				delete q;
-				q = NULL;
+				Node *q;
+
+				while (head[i])
+				{
+					q = head[i];
+					sum += q->count;
+					cout << q->value << " = " << q->count << endl;
+					head[i] = head[i]->next;
+				}
 			}
-		}
-	delete[] hashtable;
-}
+		cout << "The total amount: " << sum << endl;
+	}
+	List()
+	{
+		head = new Node *[Hash_size];
+		for (int i = 0; i < Hash_size; i++)
+			head[i] = NULL;
+	}
+	~List()
+	{
+		for (int i = 0; i < Hash_size; i++)
+			if (head[i])
+			{
+				Node *q;
+				while (head[i])
+				{
+					q = head[i];
+					head[i] = head[i]->next;
+					delete q;
+					q = NULL;
+				}
+			}
+		delete[] head;
+	}
+};
 
 void main()
 {
+	List list;
 	char signs[] = "-=+/*,.;:'\"><(){}[]";
 	int const n = 20; int  m = sizeof(signs) - 1;
 	char c[n];
-	Create();
 	ifstream f("input.txt");
 	if (!f)
 		cerr << "Error opening file" << endl;
@@ -105,10 +108,9 @@ void main()
 			for (int j = 0; j < m; j++)
 				if (c[i] == signs[j])
 					c[i] = '\0';
-		Insert(c);
+		list.Insert(c);
 	}
 
-	Print();
-	Remove();
+	list.Print();
 	system("pause");
 }
