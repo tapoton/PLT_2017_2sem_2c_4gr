1c776094a25985e2b20441c7da5754bf35d56bc5
Kornshnaps <pugachev1997@bk.ru>
2017-05-15 18:40:46 +0300

    Add files via upload

--- pugachev/lab2.cpp
+++ pugachev/lab2.cpp
@@ -4,63 +4,125 @@
 
 using namespace std;
 
-const int  n = 5;
+const int  n = 15;
 
 class hashtabl
 {
 public:
 	struct spisok
 	{
+		int kolvo;
 		string word;
 		spisok* next;
 	};
 	spisok* tabl = new spisok[n];
-		
+
 	hashtabl()
 	{
 		for (int i = 0; i < n; i++)
-			tabl[i].next = NULL;
+		{
+		tabl[i].next = NULL;
+
+		}
 	}
-		
-	void insert(string wordp)
+
+	/*void insert(string wordp)
+	{
+	int k = 0;
+	spisok* newsp;
+	//bool check = 0;
+	for (int i = 0; i<n; i++)
+	{
+	newsp = tabl[i].next;
+	while (newsp)
+	{
+	if (wordp == newsp->word)
+	k++;
+	newsp = newsp->next;
+	}
+
+	}
+	if (k == n)
+	return;
+	newsp = &tabl[k];
+	while (newsp->next)
+	{
+	newsp = newsp->next;
+	}
+	spisok* paste = new spisok;
+	paste->word = wordp;
+	newsp->next = paste;
+	paste->next = NULL;
+	return;
+	}*/
+
+	/*void printstrok(int i)
+	{
+	spisok* newsp = tabl[i].next;
+	while (newsp)
+	{
+	cout << newsp->word << ' ';
+	newsp = newsp->next;
+	}
+	};*/
+
+
+
+	int hashfunc(string word)
 	{
 		int k = 0;
-		spisok* newsp;
-		//bool check = 0;
-		for (int i=0;i<n;i++)
-		{
-			newsp = tabl[i].next;
-			while (newsp)
-			{
-				if (wordp == newsp->word)
-					k++;
-				newsp = newsp->next;
-			}
-
-		}
-		if (k == n)
-			return;
-		newsp = &tabl[k];
-			while (newsp->next)
-			{
-				newsp = newsp->next;
-			}
-		spisok* paste = new spisok;
-		paste->word = wordp;
-		newsp->next = paste;
-		paste->next = NULL;
-		return;
+		for (int i = 0; i < word.length(); i++)
+			k += (int)word[i];
+		return k % n;
 	}
 
-	void printstrok(int i)
+	void insert(string pushword)
 	{
-		spisok* newsp = tabl[i].next;
-		while (newsp)
+		int key = hashfunc(pushword);
+		spisok* newsp = &tabl[key];
+		bool check = 0;
+		while (newsp->next)
 		{
-			cout << newsp->word << ' ';
 			newsp = newsp->next;
+			if (newsp->word == pushword)
+			{
+				check = 1;
+				newsp->kolvo++;
+				return;
+			}
+			
 		}
+		if (!check)
+		{
+			spisok* paste = new spisok;
+			paste->word = pushword;
+			paste->kolvo = 1;
+			newsp->next = paste;
+			newsp->next->next = NULL;
+		}
+
+		/*{
+		newsp->word = pushword;
+		newsp->next = NULL;
+		newsp->kolvo = 1;
+		}*/
+
 	};
+	void search(int kolvo)
+	{
+		spisok* newsp;
+		for (int i = 0; i<n; i++)
+		{
+			newsp = &tabl[i];
+			while (newsp)
+			{
+				if (newsp->kolvo >= kolvo)
+					cout << newsp->word << " ";
+				newsp = newsp->next;
+			}
+			
+		}
+	}
 
 };
 
@@ -81,8 +143,8 @@ int main()
 				word.clear();
 			}
 		}
-	
-	newht.printstrok(4);
-	system("pause"); 
+	newht.search(4);
+	//newht.printstrok(4);
+	system("pause");
 	return 0;
 }
